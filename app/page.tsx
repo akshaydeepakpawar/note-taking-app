f5d007ddbba3727d5c47f4df5003344664f4be7c
akshay pawar <ap3690700@gmail.com>
2026-03-20 23:57:42 +0530

    database connected

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-import Image from "next/image";
 import dbConnect from "@/lib/db"
+import NotesClient from "@/components/NotesClient"
 
 export default async function Home() {
 
   await dbConnect();
 
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>hello db is connected</h1>
-    </div>
+        <div className="container mx-auto p-4">
+          <h1 className="text-3xl font-bold mb-6">Notes App</h1>
+          <NotesClient/>
+        </div>
   );
 }
 
